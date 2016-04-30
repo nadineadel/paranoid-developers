95514d4eef8e846612a5fdae4468d30c328c018c
nadine <nadineadelzayed@gmail.com>
2016-04-30 15:05:46 +0200

    fix

--- register.py
+++ register.py
@@ -1,10 +1,8 @@
 import sqlite3
-ر=sqlite3.connect
-def ॺ(username,password,email):
- with ر("milestone2.db")as conn:
-  ڤ=conn.cursor()
-  蝆=ڤ.execute("INSERT INTO user (username, password, email) VALUES ('{username}','{password}','{email}')".format(username=username,password=password,email=email))
-  conn.commit()
-  conn.close()
- return "user added"
-# Created by pyminifier (https://github.com/liftoff/pyminifier)
+def register(username, password, email):
+  with sqlite3.connect("milestone2.db") as conn:
+    c = conn.cursor()
+    new_user = c.execute("INSERT INTO user (username, password, email) VALUES ('{username}','{password}','{email}')".format(username = username, password = password, email = email))
+    conn.commit()
+    conn.close()
+  return "user added"
--- update.py
+++ update.py
@@ -1,21 +1,20 @@
 import argparse
 import sqlite3
-ﶄ=sqlite3.connect
-def 㸛(username,change,password_email):
- if change=="password":
-  with ﶄ("milestone2.db")as conn:
-   闑=conn.cursor()
-   ﲷ=闑.execute("UPDATE user SET password = '{password}' WHERE username = '{username}';".format(username=username,password=password_email))
-   conn.commit()
-   return "password updated"
- elif change=="email":
-  with ﶄ("milestone2.db")as conn:
-   闑=conn.cursor()
-   ﲷ=闑.execute("UPDATE user SET email = '{email}' WHERE username = '{username}';".format(username=username,email=password_email))
-   conn.commit()
-   return "email updated"
- else:
-  return "you can only update password and email"
-if __name__=="__main__":
- 㸛()
-# Created by pyminifier (https://github.com/liftoff/pyminifier)
+def update(username, change, password_email):
+  if change == "password":
+    with sqlite3.connect("milestone2.db") as conn:
+      c = conn.cursor()
+      update_user = c.execute("UPDATE user SET password = '{password}' WHERE username = '{username}';".format(username = username, password = password_email))
+      conn.commit()
+      return "password updated"
+  elif change == "email":
+    with sqlite3.connect("milestone2.db") as conn:
+      c = conn.cursor()
+      update_user = c.execute("UPDATE user SET email = '{email}' WHERE username = '{username}';".format(username = username, email = password_email))
+      conn.commit()
+      return "email updated"
+  else:
+    return "you can only update password and email"
+
+if __name__ == "__main__":
+  update()
