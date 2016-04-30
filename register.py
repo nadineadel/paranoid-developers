9b856de07020520ddf76368b28403ac25cc7e2cd
nadine <nadineadelzayed@gmail.com>
2016-04-30 15:01:02 +0200

    remove encrypted files

--- register.py
+++ register.py
@@ -1,8 +1,10 @@
 import sqlite3
-def register(username, password, email):
-  with sqlite3.connect("milestone2.db") as conn:
-    c = conn.cursor()
-    new_user = c.execute("INSERT INTO user (username, password, email) VALUES ('{username}','{password}','{email}')".format(username = username, password = password, email = email))
-    conn.commit()
-    conn.close()
-  return "user added"
+ر=sqlite3.connect
+def ॺ(username,password,email):
+ with ر("milestone2.db")as conn:
+  ڤ=conn.cursor()
+  蝆=ڤ.execute("INSERT INTO user (username, password, email) VALUES ('{username}','{password}','{email}')".format(username=username,password=password,email=email))
+  conn.commit()
+  conn.close()
+ return "user added"
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
